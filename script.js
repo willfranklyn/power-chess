7e36b2f85f5ca06ff49d2929cbf08a86652cd65b
willfranklyn <willfranklyn2@gmail.com>
2023-10-02 16:13:46 -0700

    Remove ToolTip and Fix API Call

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-async function fetchData() {
+async function fetchData(username) {
     const options = {
         method: 'GET'
     }
-
-    const res = await fetch("https://api.chess.com/pub/player/willfranklyn", options);
+    const res = await fetch(`https://api.chess.com/pub/player/${username}`, options);
     const record = await res.json();
 
-    console.log("record: ", record);
-    console.log("League: ", record.league);
-    document.getElementById("league").innerHTML = record.league;
+    if (record.code == 0) {
+        const error = document.getElementById("username-error");
+        error.style.visibility = "visible";
+        error.innerHTML = "Error Retrieving Username";
+    } else {
+        console.log("record: ", record);
+        console.log("League: ", record.league);
+        document.getElementById("league").innerHTML = record.league;
+    }
+    
+    
 }
 
-fetchData();
+function buttonSubmit() {
+    const username = document.getElementById("username-input").value;
+    console.log("username: ", username);
+    if (username) {
+        fetchData(username);
+    } else {
+        const error = document.getElementById("username-error");
+        error.style.visibility = "visible";
+        error.innerHTML = "Error Retrieving Username Data";
+    }
+    
+}
